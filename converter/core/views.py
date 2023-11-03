3c6e33a7c23e196947d4c25c76fc6f1504ee58d7
Andrew <elbroandrew@gmail.com>
2023-11-03 17:20:42 +1000

    add form for image uploading

--- converter/core/views.py
+++ converter/core/views.py
@@ -1,18 +1,26 @@
-from flask import render_template, request, Blueprint
-
-import socket
+import pathlib
+from flask import render_template, request, Blueprint, redirect, url_for
+from converter.core.forms import ImageForm
+from werkzeug.utils import secure_filename
 
 core = Blueprint('core', __name__)
 
-@core.route('/')
-def index():
+@core.route('/index', methods=['GET', 'POST'])
+def upload_image():
     '''
     This is the home page view. Notice how it uses pagination to show a limited
     number of posts by limiting its query size and then calling paginate.
     '''
-    hostname = socket.gethostname()
-    host_ip = socket.gethostbyname(hostname)
-    return render_template('index.html', HOSTNAME=hostname, IP=host_ip)
+    form = ImageForm()
+    if form.validate_on_submit():
+        img = form.image.data
+        filename = secure_filename(img.filename)
+        project_dir = pathlib.Path(__file__).resolve()
+        save_dir = file_path.parent.parent/"assets"/filename
+        img.save(save_dir)
+        return redirect(url_for('core.index'))
+
+    return render_template('index.html', form=form)
 
 @core.route('/info')
 def info():
@@ -21,3 +29,4 @@ def info():
     contact page. Any page that doesn't really sync with one of the models.
     '''
     return render_template('info.html')
+
