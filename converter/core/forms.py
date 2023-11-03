c82c9de28d8b8bb600b5d924d06e284f5b3c27b8
Andrew <elbroandrew@gmail.com>
2023-11-03 17:24:04 +1000

    add validator only blp files allowed

--- converter/core/forms.py
+++ converter/core/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 
 class ImageForm(FlaskForm):
-    image = FileField(validators=[FileRequired()])
+    image = FileField('image', validators=[
+        FileRequired(),
+        FileAllowed(['blp'], ".BLP files only required!")
+    ])
 
  
