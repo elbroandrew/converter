5013cf1cdcfd5d5832ccf850859413dc5bcff9d5
andrew <elbroandrew@gmail.com>
2023-11-05 14:00:36 +1000

    refactor: add download button

--- api/core/views.py
+++ api/core/views.py
@@ -1,33 +1,38 @@
 import pathlib
-from flask import render_template, Blueprint, redirect, url_for, flash
+from flask import render_template, Blueprint, redirect, url_for, flash, request
 from api.core.forms import ImageForm
 from werkzeug.utils import secure_filename
 
 core = Blueprint('core', __name__)
 
-@core.route('/', methods=[ 'POST'])
+@core.route('/', methods=['GET', 'POST'])
 def upload_image():
     '''
     This is the home page view.
     '''
+    btn=False
     form = ImageForm()
-    if form.validate_on_submit():
-        img = form.image.data
-        filename = secure_filename(img.filename)
-        project_dir = pathlib.Path(__file__).resolve()
-        save_path = project_dir.parent.parent/"assets"/filename
-        try:
-            img.save(save_path)
-        
-        flash("File uploaded sucessfuly.")
-        return redirect(url_for('core.index'))
+    if request.method == 'POST' and  form.validate_on_submit():
+        if form.send.data:
+            img = form.image.data
+            filename = secure_filename(img.filename)
+            project_dir = pathlib.Path(__file__).resolve()
+            save_path = project_dir.parent.parent/"assets"/filename
+            try:
+                img.save(save_path)
+                flash("File uploaded sucessfuly.")
+                btn=True
+            except Exception:
+                flash("Could not upload the file.")
+            
+        if form.download.data:
+            print("DOWNLOADING IMAGE.")
+        print(btn)
+        return render_template('index.html', form=form, btn=btn)  # redirect(url_for('core.upload_image'))
+    print("btn :%s" % btn)            
 
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, btn=btn)
 
-@core.route('/', methods=['GET'])
-def index():
-    form = ImageForm()
-    return render_template('index.html', form=form)
 
 @core.route('/info')
 def info():
