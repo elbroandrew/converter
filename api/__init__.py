db71a6092c4784436c7d7b43f55a322a20d1b224
Andrew <elbroandrew@gmail.com>
2023-11-05 22:02:39 +1000

    add csrf protection

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api.core.views import core
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000  # this limits file size for uploading to 16 MB.
+csrf = CSRFProtect(app)
 
 app.register_blueprint(core)
