481f0b6478682ab73cb4c54618600b5f586c318c
Andrew <elbroandrew@gmail.com>
2024-02-05 15:18:12 +1000

    feat: redis container connection

--- api/core/views.py
+++ api/core/views.py
@@ -1,5 +1,7 @@
 import pathlib
+import redis
 from flask import render_template, Blueprint, flash, request, send_file,  stream_with_context, Response
+from flask import g
 from api.core.forms import ImageForm, DownloadForm
 from werkzeug.utils import secure_filename
 from PIL import Image
@@ -22,12 +24,12 @@ def upload_image():
         if form.submit_send.data:
             img = request.files['image']
             filename = secure_filename(img.filename)
-            project_dir = pathlib.Path(__file__).resolve()
-            save_path = project_dir.parent.parent/"images"/filename
+            #project_dir = pathlib.Path(__file__).resolve()
+            #save_path = project_dir.parent.parent/"images"/filename
             # save multipart octet to bytes
             image_bytes = BytesIO(img.stream.read())
             image = Image.open(image_bytes)
-            filename = filename.split('.')[0]
+            # filename = filename.split('.')[0]
             try:
                 #image.save("converter/output.png")
                 return send_file(image, download_name="output.png",  as_attachment=True)
@@ -63,3 +65,25 @@ def info():
     '''
     return render_template('info.html')
 
+@core.route('/setvar')
+def setvar():
+    g.redis_client.set('foo', 'bar')
+    return "set foo"
+
+
+@core.route('/getvar')
+def getvar():
+    return g.redis_client.get('foo')
+
+
+@core.before_request
+def before_request():
+    try:
+        g.redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    except redis.exceptions.ConnectionError as err: 
+        print("Connection error occured." , err)
+        
+
+
+# register a 'before request handler' & 'teardown request'
+core.before_request(before_request)
--- converter/converter.py
+++ converter/converter.py
@@ -1,27 +1,24 @@
 from PIL import Image
 from io import BytesIO
+import redis
+
 
 # im = Image.open("api/assets/CW_Galen_Trollbane.blp")
 
 # im_png = im.save("converter/results/output.png")
 
-class BLP_to_PNG_converter:
 
-    def __init__(self, ) -> None:
-        pass
+def img_to_bytes(self, img_path: str) -> bytes:
+    im = Image.open(img_path)
+    # im_resize = im.resize((500, 500))
+    buf = BytesIO()
+    # im_resize.save(buf, format=im.format)
+    im.save(buf, format=im.format)  # save to buffer, not disk
+    byte_im: bytes = buf.getvalue()
+    return byte_im
 
 
-    def img_to_bytes(self, img_path: str) -> bytes:
-        im = Image.open(img_path)
-        # im_resize = im.resize((500, 500))
-        buf = BytesIO()
-        # im_resize.save(buf, format=im.format)
-        im.save(buf, format=im.format)  # save to buffer, not disk
-        byte_im: bytes = buf.getvalue()
-        return byte_im
-
-    
-    def bytes_to_image(self, byte_im: bytes):
-        new_image = Image.open(BytesIO(byte_im))
-        new_image.save("OUTPUT.png")
+def bytes_to_image(self, byte_im: bytes):
+    new_image = Image.open(BytesIO(byte_im))
+    new_image.save("OUTPUT.png")
 
