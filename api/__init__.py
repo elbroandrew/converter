f8146725ba9d5a3f607505f3cc172282e254f041
Andrew <elbroandrew@gmail.com>
2024-02-15 10:24:39 +1000

    current changes 2

--- api/__init__.py
+++ api/__init__.py
@@ -7,6 +7,7 @@ from flask_wtf import CSRFProtect
 from celery import Celery
 
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'  # it is for client session cookie; if I use Flask-Session I don't need the 'secret' key. Cannot be deleted. 
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000  # this limits file size for uploading to 16 MB.
