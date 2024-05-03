84d442a133950392bf1d469d234a2636d8742c28
Andrew <elbroandrew@gmail.com>
2024-05-03 18:36:52 +1000

    include tasks file to avoid error

--- celeryapp/celery_worker.py
+++ celeryapp/celery_worker.py
@@ -1,7 +1,7 @@
 from celery import Celery
 
 
-celery_app = Celery(__name__, backend="redis://0.0.0.0:6379/0", broker="redis://0.0.0.0:6379/0")
+celery_app = Celery(__name__, backend="redis://0.0.0.0:6379/0", broker="redis://0.0.0.0:6379/0", include=["converter"])
 celery_app.set_default()
 celery_app.conf.task_track_started = True
 celery_app.conf.ignore_result = False
