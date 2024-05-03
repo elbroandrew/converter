3deee33f306c4a50a225b43b151ebeab2affff96
Andrew <elbroandrew@gmail.com>
2024-05-03 11:47:19 +1000

    make async fetch request

--- api/static/fetchpage.js
+++ api/static/fetchpage.js
@@ -1,5 +1,12 @@
-function fetch_data(){
-    fetch("/fetchtest").then(response => response.json()).then(function(data){
-        document.getElementById("to_change").innerHTML = data['some text'];
-    });
+// function fetch_data(){
+//     fetch("/fetchtest").then(response => response.json()).then(function(data){
+//         document.getElementById("to_change").innerHTML = data['some text'];
+//     });
+// }
+
+async function fetch_data(){
+    const resp = await fetch("/fetchtest");
+    const data = await resp.json();
+    document.getElementById("to_change").innerHTML = data['some text'];
+    
 }
