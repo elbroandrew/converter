0dc564dc101d3ea5c9ef4f1a7ae37eb36ab0a83d
Andrew <elbroandrew@gmail.com>
2024-05-04 17:52:59 +1000

    set serializer to pickle because filestorage obj is not json serializable

--- celeryapp/celery_worker.py
+++ celeryapp/celery_worker.py
@@ -5,3 +5,5 @@ celery_app = Celery(__name__, backend="redis://0.0.0.0:6379/0", broker="redis://
 celery_app.set_default()
 celery_app.conf.task_track_started = True
 celery_app.conf.ignore_result = False
+celery_app.conf.task_serializer = 'pickle'
+celery_app.conf.result_serializer = 'pickle'
