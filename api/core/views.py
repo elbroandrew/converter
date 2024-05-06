ccfd15d7cfe1f9cb8491677d0383c132ce115286
Andrew <elbroandrew@gmail.com>
2024-05-06 12:59:45 +1000

    redirect via fetch api to homepage on download btn click

--- api/core/views.py
+++ api/core/views.py
@@ -1,11 +1,9 @@
-from flask import render_template, Blueprint, flash, request, session, jsonify, make_response, app
+from flask import render_template, Blueprint, flash, request, session, jsonify, redirect, url_for
 from api.core.forms import ImageForm
 from werkzeug.utils import secure_filename
-from celery.result import AsyncResult
 from converter import save_png_bytes_to_redis
 from celeryapp.celery_worker import celery_app
 import time
-from uuid import uuid4
 import pprint
 from io import BytesIO
 from celery import current_app
@@ -18,59 +16,50 @@ core = Blueprint('core', __name__)
 def upload_image():
 
     form = ImageForm()
+    display_download = False
+    task_id = ""
     if request.method == 'POST' and  form.validate_on_submit():
 
         session.permanent = True
 
         if form.submit_send.data:
 
-            user_uuid = str(uuid4())
-            session["user_uuid"] = user_uuid
-
-            img = request.files['image']
-            secure_filename(img.filename)
-            # save multipart octet to bytes
-            img_bytes = BytesIO(img.stream.read())
-            task = save_png_bytes_to_redis.delay(img_bytes.getvalue())
-            session["task_id"] = str(task.id)
-            print("TASK ID: ", task.id)
-            print("TASK RESULT: ", task.result)
-
-            task_result = save_png_bytes_to_redis.AsyncResult(task.id)
-            print("result state: ", task_result.state)
-            print("result:", task_result.result)
-            img_bytes.close()
             try:
+                img = request.files['image']
+                secure_filename(img.filename)
+                # save multipart octet to bytes
+                img_bytes = BytesIO(img.stream.read())
+                task = save_png_bytes_to_redis.delay(img_bytes.getvalue())
+                session["task_id"] = str(task.id)
+                task_id = str(task.id)
+                display_download=True
+                print("TASK ID: ", task.id)
+                print("TASK RESULT: ", task.result)
+
+                task_result = save_png_bytes_to_redis.AsyncResult(task.id)
+                print("result state: ", task_result.state)
+                print("result:", task_result.result)
+                img_bytes.close()
                 flash("File uploaded sucessfuly.", category='success')
+                
             except Exception as e:
                 flash("Could not upload the file.", category='error')
-        
-        
 
-        return render_template('index.html', form=form)  # redirect(url_for('core.upload_image'))
-
-
-    # if request.method == 'GET':  # testing getting cookies
-    #     try:
-    #         theme = request.cookies.get("theme")
-    #         print(theme)
-    #         session["username"] = "Andrew"
-            
-    #     except Exception:
-    #         print("No such cookie.")
-
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, display_download=display_download, taskId=task_id)
 
 
 @core.route('/info')
 def info():
     # resp.set_cookie("theme", "dark")
     # print(session.get("username", None))
-    _task_id_ = session['task_id']
-    task = current_app.AsyncResult(_task_id_)
-    print(current_app.AsyncResult(_task_id_))
-    print(task.get())
-    return render_template('info.html', taskId=_task_id_)
+    if session.get('task_id'):
+        _task_id_ = session['task_id']
+        task = current_app.AsyncResult(_task_id_)
+        print(current_app.AsyncResult(_task_id_))
+        print(task.get())
+        return render_template('info.html', taskId=_task_id_)
+    
+    return render_template('info.html', taskId="PNG IMAGE IS NOT READY YET.")
 
 
 
@@ -106,3 +95,8 @@ def run_task():
 def fetchtest():
     time.sleep(3)
     return {"some text": "fetch worked!"}
+
+@core.route("/fetchpng", methods=["GET"])
+def fetch_png():
+    time.sleep(2)
+    return redirect(url_for('core.upload_image'))
