030609dc7c9dfeb28865d06758ffcc80f3cd9cae
Andrew <elbroandrew@gmail.com>
2024-05-07 09:49:26 +1000

    fix: samesite cookie issue

--- api/__init__.py
+++ api/__init__.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'  # it is for client session cookie; if I use Flask-Session I don't need the 'secret' key. Cannot be deleted. 
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000  # this limits file size for uploading to 16 MB.
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=3)
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
 csrf = CSRFProtect(app)
 
