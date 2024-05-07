e8f3463b63d516ac88a38d8d78564e1840edc565
Andrew <elbroandrew@gmail.com>
2024-05-07 16:06:58 +1000

    сделал свой трейс, т.к. логируется в консоль все png_bytes из ф-ии

--- celeryapp/celery_worker.py
+++ celeryapp/celery_worker.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.app import trace
 
 
 celery_app:Celery = Celery(__name__, backend="redis://0.0.0.0:6379/0", broker="redis://0.0.0.0:6379/0", include=["converter"])
@@ -10,3 +11,7 @@ celery_app.conf.result_serializer = 'json'
 celery_app.conf.event_serializer = 'json'
 celery_app.conf.accept_content = ['application/json', 'application/x-python-serialize', 'pickle']
 celery_app.conf.result_accept_content = ['application/json', 'application/x-python-serialize', 'pickle']
+
+trace.LOG_SUCCESS="""
+Task %(name)s[%(id)s] succeeded in %(runtime)ss\
+"""
