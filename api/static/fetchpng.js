7cfe4d06b566d69504b685fb3d409b8846454c60
andrew <elbroandrew@gmail.com>
2024-05-08 14:31:28 +1100

    сделал костыльную загрузку через fetch, но загружает по нажатию автоматически

--- api/static/fetchpng.js
+++ api/static/fetchpng.js
@@ -4,11 +4,21 @@ async function fetch_png(){
         // const data = await resp.json();
         // document.getElementById("to_change").innerHTML = "DONE!";
         // window.location.href = "/";
+        console.log(resp)
         const blob = await resp.blob();
         const file = window.URL.createObjectURL(blob);
         // window.location.assign(file);
-        window.open(file);
-        window.location.href = "/";
+        // window.open(file);
+        // window.location.href = "/";
+
+        let link = document.createElement("a");
+        link.href = file;
+        link.download = "image.png";
+        link.style.display = "none";
+        document.body.appendChild(link);
+        link.click();
+        window.URL.revokeObjectURL(file);
+        document.body.removeChild(link);
         
     }else{
         console.log("ERROR: cannot download PNG image. HTTP error: " + resp.status);
