163cb8af98c9346b06142daed379c55340d3e9a6
Andrew <elbroandrew@gmail.com>
2024-06-03 16:03:36 +1000

    тест клиент для каждого теста будет использован

--- tests/test_home.py
+++ tests/test_home.py
@@ -4,8 +4,14 @@ import pytest
 
 @pytest.mark.usefixtures("init_client", "hello")
 class TestHome():
-    def test_home(self, init_client):
-        resp = init_client.get("/")
+
+    # add fixture 'init_cleint' for every test inside this class
+    @pytest.fixture(autouse=True)
+    def client(self, init_client):
+        self.client = init_client
+
+    def test_home(self):
+        resp = self.client.get("/")
         assert resp.status_code ==200
 
     def test_a(self):
